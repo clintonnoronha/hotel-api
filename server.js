4949bc0b4ab7166a1b946dfe479f0699874924ba
Clinton Noronha <noronhaclinton@proton.me>
2023-09-16 11:25:15 +0530

    updated configuration for mongo db

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const userRouter = require("./routes/user");
 const app = express();
 
 const PORT = process.env.PORT;
-const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_USER_SECRET}@cluster0.bxlea0m.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_USER_SECRET}@cluster0.bxlea0m.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGO_URL).then(() => {
   console.log("Connected to MongoDB...");
